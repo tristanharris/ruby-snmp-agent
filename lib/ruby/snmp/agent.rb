d20d8a6889115433d81bf4b62bcba2134f65e570
Matti-Oskari Leppanen <mongrelx@gmail.com>
2015-09-03 11:29:34 +0300

    Initial commit

---
+++ lib/ruby/snmp/agent.rb
@@ -0,0 +1,9 @@
+require "ruby/snmp/agent/version"
+
+module Ruby
+  module Snmp
+    module Agent
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/ruby/snmp/agent/version.rb
@@ -0,0 +1,7 @@
+module Ruby
+  module Snmp
+    module Agent
+      VERSION = "0.1.0"
+    end
+  end
+end
